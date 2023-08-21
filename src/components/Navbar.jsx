379d6eab60b16c8bf05b085c7952bfc70b91ee09
Camilo Davila <51010598+CMOISDEAD@users.noreply.github.com>
2023-08-21 21:10:15 +0000

    ui: navbar resposive redesign

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,31 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+const Views = () => {
+  return (
+    <>
+      <li>
+        <NavLink to="/reservaciones">Reservaciones</NavLink>
+      </li>
+      <li>
+        <NavLink to="/cuartos">Cuartos</NavLink>
+      </li>
+      <li>
+        <NavLink to="/camas">Camas</NavLink>
+      </li>
+
+      <li>
+        <NavLink to="/clientes">Clientes</NavLink>
+      </li>
+    </>
+  );
+};
 
 export const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
         <div className="dropdown">
-          <label tabIndex={0} className="btn btn-circle btn-ghost">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -17,7 +37,7 @@ export const Navbar = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
+                d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
           </label>
@@ -25,75 +45,17 @@ export const Navbar = () => {
             tabIndex={0}
             className="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow"
           >
-            <li>
-              <Link to="/">Principal</Link>
-            </li>
-            <li>
-              <Link to="/reservaciones">Reservaciones</Link>
-            </li>
-            <li>
-              <Link to="/cuartos">Cuartos</Link>
-            </li>
-            <li>
-              <Link to="/camas">Camas</Link>
-            </li>
-
-            <li>
-              <Link to="/clientes">Clientes</Link>
-            </li>
+            <Views />
           </ul>
         </div>
-      </div>
-      <div className="navbar-center">
-        <Link className="btn btn-ghost text-xl normal-case" to="/">
+        <NavLink className="btn btn-ghost text-xl normal-case" to="/">
           Hotels Cocora
-        </Link>
+        </NavLink>
       </div>
-      <div className="navbar-end">
-        <button className="btn btn-circle btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-        <button className="btn btn-circle btn-ghost">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-            <span className="badge indicator-item badge-primary badge-xs">
-              0
-            </span>
-          </div>
-        </button>
-        <button className="btn btn-circle btn-ghost">
-          <div className="avatar rounded-full border border-transparent transition-colors hover:cursor-pointer hover:border-neutral-300">
-            <div className="w-8 rounded-full">
-              <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-            </div>
-          </div>
-        </button>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <Views />
+        </ul>
       </div>
     </div>
   );
